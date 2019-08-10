c57b59ddf69c089e91916b469e5409ce1f101aac
AC-Unicorn <a894657305@qq.com>
2019-08-10 16:02:20 +0800

    remove old migration files

--- db/migrate/20190206040601_create_cucsids.rb
+++
@@ -1,8 +0,0 @@
-class CreateCucsids < ActiveRecord::Migration[5.2]
-  def change
-    create_table :cucsids do |t|
-      
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190207090258_change_members.rb
+++
@@ -1,5 +0,0 @@
-class ChangeMembers < ActiveRecord::Migration[5.2]
-  def change
-    add_index :members, :CUCSID, unique:true
-  end
-end
